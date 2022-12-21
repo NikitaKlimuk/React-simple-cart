6d5fbfa224d82a61454f27ba33dcb757ada88a07
NikitaKlim <nikitaklimukwot@gmail.com>
2022-12-21 11:00:31 +0100

    0.2 final component setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,25 @@
+import Title from "../Title/Title";
+import Cart from "../Cart/Cart";
+
 import "./_base.scss";
 import "./_reset.scss";
 import "./_vars.scss";
+import "./_section-cart.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React app</h1>
-    </div>
+    <section className="section-cart">
+      <header className="section-cart__header">
+          <div className="container">
+              <Title/>
+          </div>
+      </header>
+      <div className="section-cart__body">
+          <div className="container">
+            <Cart/>
+          </div>
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,7 @@
+import "./title.scss";
+
+const Title = () => {
+    return <h1 className="title-1">Корзина товаров</h1>;
+}
+
+export default Title;

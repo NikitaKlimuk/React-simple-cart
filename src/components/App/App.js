7b5fed070049d61f707f6206163edb92d7723e97
NikitaKlim <nikitaklimukwot@gmail.com>
2022-12-21 10:07:17 +0100

    0.1 base project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./_base.scss";
+import "./_reset.scss";
+import "./_vars.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React app</h1>
+    </div>
+  );
+}
+
+export default App;
